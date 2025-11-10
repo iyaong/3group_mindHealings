1a212d5433d5b1715b9eae4a43e7158c1194c206
cukibyeol <cukibyeol@gmail.com>
2025-11-10 17:41:38 +0900

    네비게이션 모바일 지원

--- src/pages/Navigation.tsx
+++ src/pages/Navigation.tsx
@@ -3,7 +3,7 @@
 
 import { useNavigate, NavLink, useLocation } from "react-router-dom";
 import { useDisplay } from "../contexts/DisplayContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import "./Navigation.css";
 
@@ -32,6 +32,9 @@ export default function Navigation() {
 
     // 현재 경로가 "/" (메인 페이지) 인지 확인
     const isMainPage = location.pathname === "/";
+
+    // menuOpen: 모바일 메뉴 활성화 상태
+    const [displayMobileMenu, setDisplayMobileMenu] = useState(false);
 
     useEffect(() => {
         try {
@@ -72,6 +75,9 @@ export default function Navigation() {
         // 네비게이션 전체
         <nav className={`${isMainPage ? "main" : ""}`}>
             <div className="nav-left">
+                <div className="hamburger" onClick={() => setDisplayMobileMenu(!displayMobileMenu)}>
+                    ☰
+                </div>
                 <h1
                     onClick={goHome}
                     title="메인으로 이동"
@@ -85,69 +91,45 @@ export default function Navigation() {
             {user && (
                 <div className="nav-center">
                     <NavLink
-                        to="/diary"
+                        to="/diary" className="link-button"
                         style={({ isActive }) => ({
-                            padding: "6px 10px",
-                            borderRadius: 8,
-                            textDecoration: "none",
-                            color: isActive ? "#2563eb" : "#111",
-                            fontWeight: 600,
+                            color: isActive ? "#2563eb" : "#111"
                         })}
                     >
                         다이어리
                     </NavLink>
 
                     <NavLink
-                        to="/history"
+                        to="/history" className="link-button"
                         style={({ isActive }) => ({
-                            padding: "6px 10px",
-                            borderRadius: 8,
-                            textDecoration: "none",
-                            color: isActive ? "#2563eb" : "#111",
-                            fontWeight: 600,
+                            color: isActive ? "#2563eb" : "#111"
                         })}
                     >
                         히스토리
                     </NavLink>
                     <NavLink
-                        to="/goals"
+                        to="/goals" className="link-button"
                         style={({ isActive }) => ({
-                            padding: "6px 10px",
-                            borderRadius: 8,
-                            textDecoration: "none",
-                            color: isActive ? "#2563eb" : "#111",
-                            fontWeight: 600,
+                            color: isActive ? "#2563eb" : "#111"
                         })}
                     >
                         목표
                     </NavLink>
                     <NavLink
-                        to="/online"
+                        to="/online" className="link-button"
                         style={({ isActive }) => ({
-                            padding: "6px 10px",
-                            borderRadius: 8,
-                            textDecoration: "none",
                             color: isActive ? "#2563eb" : "#111",
                             fill: isActive ? "#2563eb" : "#111",
-                            fontWeight: 600,
-                            display: "flex",
-                            alignItems: "center",
-
+                            display: "flex"
                         })}
                     >
                         챗온
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M620-520q25 0 42.5-17.5T680-580q0-25-17.5-42.5T620-640q-25 0-42.5 17.5T560-580q0 25 17.5 42.5T620-520Zm-280 0q25 0 42.5-17.5T400-580q0-25-17.5-42.5T340-640q-25 0-42.5 17.5T280-580q0 25 17.5 42.5T340-520Zm140 260q68 0 123.5-38.5T684-400H276q25 63 80.5 101.5T480-260Zm0 180q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Z" /></svg>
-                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M620-520q25 0 42.5-17.5T680-580q0-25-17.5-42.5T620-640q-25 0-42.5 17.5T560-580q0 25 17.5 42.5T620-520Zm-280 0q25 0 42.5-17.5T400-580q0-25-17.5-42.5T340-640q-25 0-42.5 17.5T280-580q0 25 17.5 42.5T340-520Zm140 260q68 0 123.5-38.5T684-400H276q25 63 80.5 101.5T480-260Zm0 180q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Z" /></svg>
                     </NavLink>
                     <NavLink
-                        to="/support"
+                        to="/support" className="link-button"
                         style={({ isActive }) => ({
-                            padding: "6px 10px",
-                            borderRadius: 8,
-                            textDecoration: "none",
-                            color: isActive ? "#2563eb" : "#111",
-                            fontWeight: 600,
+                            color: isActive ? "#2563eb" : "#111"
                         })}
                     >
                         고객센터
@@ -162,19 +144,8 @@ export default function Navigation() {
                 ) : user ? (
                     <>
                         <span
+                            className="profile"
                             onClick={() => navigate('/profile')}
-                            style={{
-                                color: "#2563eb",
-                                cursor: "pointer",
-                                fontWeight: 600,
-                                padding: "6px 10px",
-                                borderRadius: 8,
-                                transition: "background 0.2s",
-                                whiteSpace: 'nowrap',
-                                overflow: 'hidden',
-                                textOverflow: 'ellipsis',
-                                maxWidth: 150,
-                            }}
                             onMouseEnter={(e) => {
                                 e.currentTarget.style.background = "#eef2ff";
                             }}
@@ -194,6 +165,88 @@ export default function Navigation() {
                     </>
                 )}
             </div>
+
+            {/* 모바일 메뉴 */}
+            {displayMobileMenu && (
+                <div className="mobile-menu">
+                    <NavLink
+                        to="/diary" className="link-button"
+                        style={({ isActive }) => ({
+                            color: isActive ? "#2563eb" : "#111"
+                        })}
+                    >
+                        다이어리
+                    </NavLink>
+
+                    <NavLink
+                        to="/history" className="link-button"
+                        style={({ isActive }) => ({
+                            color: isActive ? "#2563eb" : "#111"
+                        })}
+                    >
+                        히스토리
+                    </NavLink>
+                    <NavLink
+                        to="/goals" className="link-button"
+                        style={({ isActive }) => ({
+                            color: isActive ? "#2563eb" : "#111"
+                        })}
+                    >
+                        목표
+                    </NavLink>
+                    <NavLink
+                        to="/online" className="link-button"
+                        style={({ isActive }) => ({
+                            color: isActive ? "#2563eb" : "#111",
+                            fill: isActive ? "#2563eb" : "#111",
+                            display: "flex"
+                        })}
+                    >
+                        챗온
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M620-520q25 0 42.5-17.5T680-580q0-25-17.5-42.5T620-640q-25 0-42.5 17.5T560-580q0 25 17.5 42.5T620-520Zm-280 0q25 0 42.5-17.5T400-580q0-25-17.5-42.5T340-640q-25 0-42.5 17.5T280-580q0 25 17.5 42.5T340-520Zm140 260q68 0 123.5-38.5T684-400H276q25 63 80.5 101.5T480-260Zm0 180q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Z" /></svg>
+                    </NavLink>
+                    <NavLink
+                        to="/support" className="link-button"
+                        style={({ isActive }) => ({
+                            color: isActive ? "#2563eb" : "#111"
+                        })}
+                    >
+                        고객센터
+                    </NavLink>
+
+                    {loading ? (
+                        <span>상태 확인 중...</span>
+                    ) : user ? (
+                        <>
+                            <span
+                                className="profile"
+                                onClick={() => navigate('/profile')}
+                                onMouseEnter={(e) => {
+                                    e.currentTarget.style.background = "#eef2ff";
+                                }}
+                                onMouseLeave={(e) => {
+                                    e.currentTarget.style.background = "transparent";
+                                }}
+                                title="프로필 페이지로 이동"
+                            >
+                                {user.nickname || user.email}{" 님"}
+                            </span>
+                            <button onClick={onLogout}>로그아웃</button>
+                        </>
+                    ) : (
+                        <>
+                            <button onClick={login}>로그인</button>
+                            <button onClick={register}>회원가입</button>
+                        </>
+                    )}
+                </div>
+            )}
+
+            {/* 메뉴 밖 터치 시 닫기 */}
+            {displayMobileMenu && (
+                <div className="close-mobile-menu" onClick={() => setDisplayMobileMenu(false)}></div>
+            )}
+
         </nav>
     )
 }
